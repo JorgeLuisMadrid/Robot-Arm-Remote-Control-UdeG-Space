0c8845e694fb43b04f6786fe00c03bed317541b8
JorgeMadrid <jolumago79@gmail.com>
2022-06-16 23:08:08 -0500

    Functional Node (directions changed)

--- arm_node/arm_node.ino
+++ arm_node/arm_node.ino
@@ -29,11 +29,11 @@ int it6=30;         //Posición inicial Pinza (abierta)
 
 void joyControls( const sensor_msgs::Joy &msg){
   //Motor Base (Articulación 1)
-  if(msg.axes[0]<=-0.5){          //Movimiento hacia la izquierda
+  if(msg.axes[2]<=-0.5){          //Movimiento hacia la izquierda
     digitalWrite(in1_Base, LOW);
     digitalWrite(in2_Base, HIGH);
   }
-  else if(msg.axes[0]>=0.5){      //Movimiento hacia la derecha
+  else if(msg.axes[2]>=0.5){      //Movimiento hacia la derecha
     digitalWrite(in1_Base, HIGH);
     digitalWrite(in2_Base, LOW);
   }
@@ -63,11 +63,11 @@ void joyControls( const sensor_msgs::Joy &msg){
     }  
   }
   //Motor Codo (Articulación 3)
-  if(msg.axes[2]<=-0.5){          //Movimiento hacia abajo
+  if(msg.axes[0]<=-0.5){          //Movimiento hacia abajo
     digitalWrite(in1_Codo, LOW);
     digitalWrite(in2_Codo, HIGH);       
   }
-  else if(msg.axes[2]>=0.5){       //Movimiento hacia arriba
+  else if(msg.axes[0]>=0.5){       //Movimiento hacia arriba
     digitalWrite(in1_Codo, HIGH);
     digitalWrite(in2_Codo, LOW);
   }
@@ -76,15 +76,40 @@ void joyControls( const sensor_msgs::Joy &msg){
     digitalWrite(in2_Codo, LOW); 
   }
   //Motor Muñeca (Articulación 4)
+  if(msg.axes[5]<=-0.5){          //Movimiento hacia abajo
+    digitalWrite(in3_Muneca, LOW);
+    digitalWrite(in4_Muneca, HIGH);       
+  }
+  else if(msg.axes[5]>=0.5){       //Movimiento hacia arriba
+    digitalWrite(in3_Muneca, HIGH);
+    digitalWrite(in4_Muneca, LOW);
+  }
+  else{                           //Sin movimiento
+    digitalWrite(in3_Muneca, LOW);
+    digitalWrite(in4_Muneca, LOW); 
+  }
   //Motor Mano (Articulación 5)
+  if(msg.axes[4]>=0.9){               //Boton izquierda
+    it5 = it5 - 4;
+    //delay(50);
+    if(it5<0)
+      it5 = 0;
+  }
+  else if(msg.axes[4]<=-0.9){           //Boton derecha
+    it5 = it5 + 4;
+    //delay(50);
+    if(it5>180)
+      it5 = 180;
+  }
+  Servo_Mano.write(it5);
   //Motor Pinza
-  if(msg.buttons[0]==1 && msg.buttons[1]==0){      //Abrir
+  if(msg.buttons[0]==0 && msg.buttons[1]==1){      //Abrir
     it6 = it6 - 4;
     delay(50);
     if(it6<30)
       it6 = 30;       
   }
-  else if(msg.buttons[1]==1 && msg.buttons[0]==0){ //Cerrar
+  else if(msg.buttons[1]==0 && msg.buttons[0]==1){ //Cerrar
     it6 = it6 + 4;
     delay(50);
     if(it6>100)
@@ -96,8 +121,6 @@ void joyControls( const sensor_msgs::Joy &msg){
 ros::Subscriber<sensor_msgs::Joy> sub("joy", &joyControls );
 
 void setup() {
-  // put your setup code here, to run once:
-  //Serial.begin(9600);       //Declaración de comunicación serial
   nh.initNode();
   nh.subscribe(sub);
   pinMode(in1_Base,OUTPUT);
@@ -117,6 +140,5 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   nh.spinOnce();
 }
